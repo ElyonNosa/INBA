6321a8bf91a77b52c28c568d712e39a140dcea93
Josh <jsig885@gmail.com>
2023-02-11 16:44:47 -0800

    Add ViewActivity
    For some reason

--- app/src/main/java/comp3350/inba/presentation/ProfileActivity.java
+++ app/src/main/java/comp3350/inba/presentation/ProfileActivity.java
@@ -1,22 +1,54 @@
 package comp3350.inba.presentation;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import comp3350.inba.R;
+import comp3350.inba.business.AccessTransactions;
+import comp3350.inba.objects.Transaction;
 
+import androidx.annotation.NonNull;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import comp3350.inba.R;
+import java.util.List;
 
-public class ProfileActivity extends AppCompatActivity {
 
+
+public class ProfileActivity extends Activity {
+
+    // the transactions database
+    private AccessTransactions accessTransactions;
+    // the adapter to display transactions in a list view
+    private ArrayAdapter<Transaction> transactionArrayAdapter;
+    // the local list of transactions
+    private List<Transaction> transactionList;
+
+    /**
+     * Constructor
+     * @param savedInstanceState Bundle.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        accessTransactions = new AccessTransactions();
+        try {
+            // display transactions in list
+            transactionList = accessTransactions.getTransactions();
+            transactionArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, transactionList);
+            final ListView listView = findViewById(R.id.transaction_list);
+            // adapt the transactions list to the listview
+            listView.setAdapter(transactionArrayAdapter);
+        }
+        catch (final Exception e) {
+            Messages.fatalError(this, e.getMessage());
+        }
 
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
@@ -56,5 +88,24 @@ public class ProfileActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }//OnCreate
+
+    /**
+     * Destructor
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    /**
+     * This function runs when the user returns to this page.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        transactionList = accessTransactions.getTransactions();
+        // update the transaction list
+        transactionArrayAdapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/comp3350/inba/presentation/ViewActivity.java
@@ -0,0 +1,16 @@
+package comp3350.inba.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import comp3350.inba.R;
+
+public class ViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+}
