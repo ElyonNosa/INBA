43d447e722f90eb0a6a1ff79c65ddd131370860c
index_b <barnonmt@myumanitoba.ca>
2023-03-09 15:04:39 -0500

    Changed Main

--- app/src/main/java/comp3350/inba/application/Main.java
+++ app/src/main/java/comp3350/inba/application/Main.java
@@ -2,7 +2,7 @@ package comp3350.inba.application;
 
 public class Main
 {
-	private static String dbName="TXN";
+	private static String dbName="INBA_DB.script";
 
 	public static void main(String[] args)
 	{
