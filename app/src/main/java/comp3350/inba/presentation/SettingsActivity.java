972878e035a62be74041498f5ead111f7c3aedf7
Darwin Ross <67281505+DarRoss@users.noreply.github.com>
2023-03-09 12:37:31 -0600

    Squashed commit of the following:
    commit c2c099563e174c6ab765bbe13e8449d54bf7797b
    Author: Darwin Ross <darwinxross@gmail.com>
    Date:   Thu Mar 9 09:40:54 2023 -0600
    
        Squashed commit of the following:
    
        commit 49767fd42ce84512f58d95bdfb3ad101cd776c55
        Author: Darwin Ross <darwinxross@gmail.com>
        Date:   Thu Mar 9 09:39:41 2023 -0600
    
            Squashed commit of the following:
    
            commit 95b582287b1c920b6f9169f5f03dae5bb3291f23
            Author: Darwin Ross <darwinxross@gmail.com>
            Date:   Wed Mar 8 23:37:47 2023 -0600
    
                Add Category Filter
    
                - The user may now filter the transaction list by category.
                - Added unit test for the category filter function.
    
            commit 75ff92ad54e6609b033ab9f179c43aaaf832e37f
            Author: Darwin Ross <darwinxross@gmail.com>
            Date:   Wed Mar 8 20:52:46 2023 -0600
    
                Add getIndexAfterDate Test
    
                - Added a unit test for the getIndexAfterDate function.
                - Modified the getIndexAfterDate function.
    
            commit afe448bcfb236ad3124ee2a0cb6a1290962fa6d1
            Author: Darwin Ross <darwinxross@gmail.com>
            Date:   Wed Mar 8 00:56:07 2023 -0600
    
                Find by Date
    
                - The user can now input a date and the transaction list will scroll to that date. This feature is still untested.
    
            commit 9f3e230f5506c229309a47c529cfd4baa580a688
            Author: Darwin Ross <darwinxross@gmail.com>
            Date:   Mon Mar 6 23:24:31 2023 -0600
    
                Convert long to LocalDateTime
    
                - All mentions of a long timestamp have been replaced with LocalDateTime stamps.
                - Test classes have been refactored to use LocalDateTime.
                - Imported Desugaring as to properly use LocalDateTime (API 26) on API 23.
    
            commit d4e71431d24180d2c90c2a30cdb5c914a612799b
            Author: Darwin Ross <darwinxross@gmail.com>
            Date:   Mon Mar 6 00:42:46 2023 -0600
    
                Smell Fixes
    
                - Added Category class.
                - Removed transaction list state in logic layer.
                - Moved transaction validation to Transaction class.
                - Moved logic functions from persistence into logic layer.
                - Updated tests.
                - Added small changes in preparation for SQL database.
    
        commit a1e9256853fc8de08b593b6a78affda1c3f64f70
        Author: Darwin Ross <darwinxross@gmail.com>
        Date:   Thu Mar 9 09:14:28 2023 -0600
    
            Add cancel button
    
            - Added a cancel button exits the threshold page.
            - The OK button also exits the threshold page.
    
        commit c5da140d06b5893834019020346628e1ae0bc9f6
        Author: NoyleAson <nosaaifesehi@gmail.com>
        Date:   Thu Mar 9 02:04:15 2023 -0600
    
            Fixed threshold bug
    
            Fixed bug that occurs when nothing is entered
    
        commit be9599754ade79ff81f23354657f18f2100cb4a6
        Author: NoyleAson <nosaaifesehi@gmail.com>
        Date:   Thu Mar 9 01:37:07 2023 -0600
    
            Threshold settings
    
        commit 794400dfeae59d2d7d00bb27097ae1167eeb51c5
        Author: NoyleAson <nosaaifesehi@gmail.com>
        Date:   Sun Mar 5 19:20:40 2023 -0600
    
            new branch

--- app/src/main/java/comp3350/inba/presentation/SettingsActivity.java
+++ app/src/main/java/comp3350/inba/presentation/SettingsActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -18,7 +20,20 @@ public class SettingsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        Button thresholdButton = findViewById(R.id.threshold);
+        thresholdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openThresholdActivity();
+            }
+        });
+
         navigationBarInit();
+    }
+
+    public void openThresholdActivity(){
+        Intent intent = new Intent(this, ThresholdActivity.class);
+        startActivity(intent);
     }
 
     protected void navigationBarInit() {
