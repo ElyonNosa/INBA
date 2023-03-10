0ee7320659b5b94f0a858a6d4697b12e6d2ecf71
Adwait Pujari <adwaitpujari08@gmail.com>
2023-03-10 01:39:16 -0600

    End of day Commit (Settings page up to date)
    -Added a setting page
    -Created a listView to display the options available on the settings page
    -Modified Elyons spending threshol and merged with the current settings page.
    -Merged threshold code from Elyons branch

---
+++ app/src/main/java/comp3350/inba/objects/User.java
@@ -0,0 +1,40 @@
+package comp3350.inba.objects;
+
+/**
+ * This class holds information pertaining to the user
+ **/
+public class User {
+    private String userName;
+    private double wkdayThresh;
+    private double wkendThresh;
+
+    public User(){
+        userName = "";
+        wkdayThresh = 0.0;
+        wkendThresh = 0.0;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public double getWkdayThresh() {
+        return wkdayThresh;
+    }
+
+    public double getWkendThresh() {
+        return wkendThresh;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setWkdayThresh(double wkdayThresh) {
+        this.wkdayThresh = wkdayThresh;
+    }
+
+    public void setWkendThresh(double wkendThresh) {
+        this.wkendThresh = wkendThresh;
+    }
+}
--- app/src/main/java/comp3350/inba/presentation/SettingsActivity.java
+++ app/src/main/java/comp3350/inba/presentation/SettingsActivity.java
@@ -5,20 +5,74 @@ import androidx.annotation.NonNull;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 import comp3350.inba.R;
 
 public class SettingsActivity extends Activity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
         navigationBarInit();
+
+        listView = (ListView)findViewById(R.id.listview);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Set Threshold Limit");
+        arrayList.add("Update Account details");
+        arrayList.add("Change Theme");
+        arrayList.add("Print Monthly Report");
+        arrayList.add("Submit Feedback");
+        arrayList.add("Delete Account");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                TextView textView = (TextView) super.getView(position, convertView, parent);
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20); // Set the font size to 20sp
+                textView.setTextColor(getColor(R.color.colorWhite));
+                return textView;
+            }
+        };
+
+        listView.setAdapter(arrayAdapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String) parent.getItemAtPosition(position);
+                if (selectedItem.equals("Set Threshold Limit")) {
+                    // Open the button
+                    openThresholdActivity();
+                }
+                // Perform other actions based on the clicked item
+                Toast.makeText(getApplicationContext(), "Clicked: " + selectedItem, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    protected void openThresholdActivity(){
+        Intent intent = new Intent(this, ThresholdActivity.class);
+        startActivity(intent);
     }
 
     protected void navigationBarInit() {
