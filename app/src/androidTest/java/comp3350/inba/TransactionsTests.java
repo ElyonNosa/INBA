17a2078e085b412119bc376c5826178910353edb
index_b <barnonmt@myumanitoba.ca>
2023-04-06 15:42:21 -0400

    added sys test

--- app/src/androidTest/java/comp3350/inba/TransactionsTests.java
+++ app/src/androidTest/java/comp3350/inba/TransactionsTests.java
@@ -25,6 +25,7 @@ import androidx.test.runner.AndroidJUnit4;
 
 // Presentation layer
 import comp3350.inba.presentation.DashboardActivity;
+//import comp3350.inba.presentation.TransactionsActivity;
 
 @RunWith( AndroidJUnit4.class )
 @LargeTest
@@ -36,6 +37,7 @@ public class TransactionsTests
 
     @Rule
     public ActivityTestRule<DashboardActivity> activity_rule = new ActivityTestRule<>(DashboardActivity.class);
+    //public ActivityTestRule<TransactionsActivity> other_activity_rule = new ActivityTestRule<>(TransactionsActivity.class);
 
     @Test
     public void createTransaction()
@@ -47,7 +49,13 @@ public class TransactionsTests
         onView( withId( R.id.login_button )).perform( click() );
 
         // add a transaction
-        
+        onView( withId( R.id.buttonAddTransaction )).perform( click() );
+        onView( withId( R.id.editTransactionCategory )).perform(typeText("something fun"));
+        onView( withId( R.id.editTransactionPrice )).perform(typeText("20.34"));
+        onView( withId( R.id.buttonTransactionCreate )).perform( click() );
+
         // Verify it was added
+        onData(anything()).inAdapterView(withId(R.id.listTransactions)).atPosition( 0 ).perform(click());
+        onView(withId(R.id.editTransactionCategory)).check(matches(withText("something fun")));
     }
 }
