1178ae339b5f87dcda941bad3f6977dc20486a4e
Darwin Ross <darwinxross@gmail.com>
2023-04-07 17:33:01 -0500

    Fix add TXN test
    - The add transaction system test now works.

--- app/src/androidTest/java/comp3350/inba/TransactionsTests.java
+++ app/src/androidTest/java/comp3350/inba/TransactionsTests.java
@@ -4,7 +4,6 @@ package comp3350.inba;
 import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -12,13 +11,19 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 // J unit Imports
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.anything;
+import static org.junit.Assert.assertTrue;
 
 // Android imports
+import android.view.View;
+import android.widget.ListView;
+
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -40,8 +45,9 @@ public class TransactionsTests
     //public ActivityTestRule<TransactionsActivity> other_activity_rule = new ActivityTestRule<>(TransactionsActivity.class);
 
     @Test
-    public void createTransaction()
+    public void testCreateTransaction()
     {
+        final int[] numItems = new int[1];
         // login
         onView( withId( R.id.username_edit_text )).perform(typeText("rossD"));
         onView( withId( R.id.password_edit_text )).perform( typeText( "rassit" ) );
@@ -51,11 +57,39 @@ public class TransactionsTests
         // add a transaction
         onView( withId( R.id.buttonAddTransaction )).perform( click() );
         onView( withId( R.id.editTransactionCategory )).perform(typeText("something fun"));
+        closeSoftKeyboard();
         onView( withId( R.id.editTransactionPrice )).perform(typeText("20.34"));
+        closeSoftKeyboard();
         onView( withId( R.id.buttonTransactionCreate )).perform( click() );
 
-        // Verify it was added
-        onData(anything()).inAdapterView(withId(R.id.listTransactions)).atPosition( 0 ).perform(click());
+        // count the number of items in the list
+        onView(withId(R.id.listTransactions)).check(matches(new TypeSafeMatcher<View>() {
+            /**
+             * Generates a description of the object.  The description may be part of a
+             * a description of a larger object of which this is just a component, so it
+             * should be worded appropriately.
+             *
+             * @param description The description to be built or appended to.
+             */
+            @Override
+            public void describeTo(Description description) {
+                // do nothing
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ListView listView = (ListView) view;
+
+                //here we assume the adapter has been fully loaded already
+                numItems[0] = listView.getAdapter().getCount();
+
+                return true;
+            }
+        }));
+
+        // Verify it was added as the last item in the list
+        onData(anything()).inAdapterView(withId(R.id.listTransactions)).atPosition(numItems[0] - 1).perform(click());
         onView(withId(R.id.editTransactionCategory)).check(matches(withText("something fun")));
+        onView(withId(R.id.editTransactionPrice)).check(matches(withText("20.34")));
     }
 }
