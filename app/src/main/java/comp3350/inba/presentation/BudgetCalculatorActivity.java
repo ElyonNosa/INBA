5b86f71939c64937c612961fb07f9ddae3a2b243
Adwait Pujari <adwaitpujari08@gmail.com>
2023-04-10 12:37:56 -0500

    Complete Budget Goal feature
    -Can find this feature under the settings tab
    -Added more constraint to the feature

--- app/src/main/java/comp3350/inba/presentation/BudgetCalculatorActivity.java
+++ app/src/main/java/comp3350/inba/presentation/BudgetCalculatorActivity.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -19,21 +20,72 @@ public class BudgetCalculatorActivity extends Activity {
 
     private EditText editTextItem;
     private EditText editTextPrice;
-    private Button buttonAdd;
-    private ListView listViewItems;
+    private EditText totalBudgetText;
+    private EditText totalDaysText;
     private ArrayList<String> itemList;
     private ArrayList<Double> priceList;
     private ArrayAdapter<String> adapter;
 
+    private double totalBudget;
+    private int numDays;
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_calculator);
 
         editTextItem = findViewById(R.id.editTextItem);
         editTextPrice = findViewById(R.id.editTextPrice);
+        totalBudgetText = findViewById(R.id.totalBudgetText);
+        totalDaysText = findViewById(R.id.totalDaysText);
 
-        buttonAdd = findViewById(R.id.buttonAdd);
+        addAndRemoveList();
+
+        Button buttonConfirm = findViewById(R.id.buttonConfirm);
+        buttonConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateBudget();
+            }
+        });
+    }
+
+
+    public void calculateBudget()
+    {
+        TextView showCalculatedBudget = findViewById(R.id.showCalculatedBudget);
+
+        Button buttonConfirm = findViewById(R.id.buttonConfirm);
+
+        String totalBudgetString = totalBudgetText.getText().toString();
+        String totalDaysString = totalDaysText.getText().toString();
+
+        if (totalBudgetString.isEmpty() || totalDaysString.isEmpty()) {
+            //If empty then return, i.e handle the error
+            return;
+        }
+
+        totalBudget = Integer.parseInt(totalBudgetString);
+        numDays = Integer.parseInt(totalDaysString);
+        buttonConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double totalPrice = 0.0;
+                for (double price : priceList) {
+                    totalPrice += price;
+                }
+
+                double remainingBudget = totalBudget - totalPrice;
+                double dailyBudget = remainingBudget / numDays;
+                showCalculatedBudget.setText("Total Days: " + numDays + ", Daily budget: $" + String.format("%.2f", dailyBudget));
+                showCalculatedBudget.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+
+    public void addAndRemoveList()
+    {
+        Button buttonAdd = findViewById(R.id.buttonAdd);
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,13 +105,14 @@ public class BudgetCalculatorActivity extends Activity {
             }
         });
 
-        listViewItems = findViewById(R.id.listViewItems);
+
+        ListView listViewItems = findViewById(R.id.listViewItems);
         itemList = new ArrayList<>();
         priceList = new ArrayList<>();
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemList);
         listViewItems.setAdapter(adapter);
 
-        // Set a long click listener on the ListView to allow items to be deleted
+        // A long click listener on the ListView to allow items to be deleted
         listViewItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
